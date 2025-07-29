aa3e3a96c90937337fae910370e7e3794ca181ea
Juan Manuel <114026096+JuanManuel1970@users.noreply.github.com>
2025-07-29 14:33:35 -0300

    Se agrega .env

--- mantenimiento/src/main/java/com/lubricentro/mantenimiento/MantenimientoApplication.java
+++ mantenimiento/src/main/java/com/lubricentro/mantenimiento/MantenimientoApplication.java
@@ -3,11 +3,11 @@ package com.lubricentro.mantenimiento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MantenimientoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MantenimientoApplication.class, args);
 	}
-
 }
